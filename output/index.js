acdea11ca577f1334be3ca1686d2478e85de91e6
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-04 16:16:35 +0600

    introduction to generic

--- output/index.js
+++ output/index.js
@@ -1,10 +1,29 @@
 "use strict";
 {
-  //
-  //* Type Assertion
-  let anything;
-  anything = "Next Level Web Development";
-  anything = 2025;
-  console.log(anything);
-  //
+    //
+    //** Type assertion */
+    let anything;
+    anything = "Next Level Web Development";
+    anything = 2025;
+    anything = true;
+    //   console.log(anything);
+    anything.toFixed;
+    const kgToGram = (value) => {
+        if (typeof value === "string") {
+            const convertedValue = parseFloat(value) * 1000;
+            return `The Converted value is : ${convertedValue}`;
+        }
+        if (typeof value === "number") {
+            return value * 1000;
+        }
+    };
+    const resultOne = kgToGram(1000);
+    const resultTwo = kgToGram("2000");
+    try {
+        //////////
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+    //
 }
---
+++ src/generic.ts
@@ -0,0 +1,52 @@
+{
+  //** Generic Type */
+
+  //   type GenericArray = Array<number>;
+  //   type GenericArray = Array<string>;
+  //   type GenericArray = Array<boolean>;
+  //   type GenericArray<param> = Array<param>;
+  type GenericArray<T> = Array<T>;
+
+  //   const rollNumber: number[] = [3, 5, 6, 8, 9];
+  //   const rollNumber: Array<number> = [3, 5, 6, 8, 9];
+  const rollNumber: GenericArray<number> = [3, 5, 6, 8, 9];
+  // const rollNumber: GenericArray = [3, 5, 6, 8, 9];
+  //   const stringArray: string[] = ["Mr. Sifat", "Mr Sajul", "Mr Ayan"];
+  // const stringArray: Array<string> = ["Mr. Sifat", "Mr Sajul", "Mr Ayan"];
+  const stringArray: GenericArray<string> = [
+    "Mr. Sifat",
+    "Mr Sajul",
+    "Mr Ayan",
+  ];
+  //   const stringArray: GenericArray = ["Mr. Sifat", "Mr Sajul", "Mr Ayan"];
+  //   const booleanArray: boolean[] = [true, false, true];
+  // const booleanArray: GenericArray = [true, false, true];
+  const booleanArray: GenericArray<boolean> = [true, false, true];
+
+  // Array of Object
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "Sifat",
+      age: 22,
+    },
+    {
+      name: "Sajul",
+      age: 26,
+    },
+  ];
+
+  const add = (x: number, y: number) => {
+    return x + y;
+  };
+
+  add(55, 66);
+
+  // ** Generic Tuple
+  type GenericTuple<X, Y> = [X, Y];
+
+  const userTuple: GenericTuple<string, string> = ["Mr X", "Mr Y"];
+  const userWithId: GenericTuple<number, { name: string; email: string }> = [
+    1234,
+    { name: "Sifat", email: "sahabulislamsifat@gmail.com" },
+  ];
+}
